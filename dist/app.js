5fa04d0ba57fe77142e0be5bdd5aed940901ed62
thisisnago <yurii.nagorniak@gmail.com>
2024-04-06 19:43:14 +0100

    Added persons getter

--- dist/app.js
+++ dist/app.js
@@ -104,6 +104,14 @@ class Component {
 }
 // ProjectItem Class
 class ProjectItem extends Component {
+    get persons() {
+        if (this.project.people === 1) {
+            return '1 person';
+        }
+        else {
+            return `${this.project.people} persons`;
+        }
+    }
     constructor(hostId, project) {
         super('single-project', hostId, false, project.id);
         this.project = project;
@@ -113,7 +121,7 @@ class ProjectItem extends Component {
     configure() { }
     renderContent() {
         this.element.querySelector('h2').textContent = this.project.title;
-        this.element.querySelector('h3').textContent = this.project.people.toString();
+        this.element.querySelector('h3').textContent = this.persons + ' assigned';
         this.element.querySelector('p').textContent = this.project.description;
     }
 }
